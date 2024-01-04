23828a78cee1af0028b0da4c4a4ea4275c7326a7
Md Antor <69227191+Antor1155@users.noreply.github.com>
2024-01-05 00:59:52 +0600

    ipad horizontal and vertical version done

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,6 +2,8 @@ import { Link } from "react-router-dom";
 import { motion } from "framer-motion"
 import languageButton from "../../assets/logos/language-options.svg";
 import cart from "../../assets/logos/cart.svg"
+import menu from "../../assets/logos/Menu.svg"
+import cancel from "../../assets/logos/Cancel.svg"
 import "./Navbar.css"
 
 const Navbar = () => {
@@ -20,16 +22,30 @@ const Navbar = () => {
         show: { opacity: 1 },
     }
 
+    const handleNavMenues = ()=>{
+        document.getElementById("nav-menues").classList.toggle("visible")
+    }
+
     return (
         <header>
             <nav>
                 <Link to="/" className="company-font"> ChaoKaiQi</Link>
 
+                <button id="menu" onClick={handleNavMenues}>
+                    <img src={menu} alt="nav bar menu icon" />
+                </button>
+
                 <motion.div
                     variants={container}
                     initial="hidden"
                     animate="show"
+
+                    id="nav-menues"
+                    onClick={handleNavMenues}
                 >
+                    <button id="cancel">
+                        <img src={cancel} alt="hide nav bar" />
+                    </button>
 
                     <motion.span variants={item} whileTap={{scale: .7}}><Link to="/">HOME</Link></motion.span>
                     <motion.span variants={item} whileTap={{scale: .7}}><Link to="/">CATAGORY</Link></motion.span>
