bc0c20d8f694e7a4e86fe54139de0546f1f1f8e9
Md Antor <69227191+Antor1155@users.noreply.github.com>
2024-01-08 02:31:28 +0600

    added privacy policy imaged but not used, privacy policy page created

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,17 @@ import Navbar from './components/Navbar/Navbar'
 import AnimatingBg from './components/AnimatingBg/AnimatingBg'
 import FooterSection from './components/FooterSection/FooterSection'
 
+import { ScrollRestoration } from "react-router-dom";
+
 function App() {
 
 
   return (
     <>
+      <ScrollRestoration />
       <AnimatingBg />
       <Navbar></Navbar>
-      <mian style={{width: "100vw", overflow: "hidden"}}>
+      <mian style={{ width: "100vw", overflow: "hidden" }}>
         <Outlet></Outlet>
       </mian>
       <FooterSection></FooterSection>
--- src/Pages/PrivacyPolicy/PrivacyPolicy.jsx
+++ src/Pages/PrivacyPolicy/PrivacyPolicy.jsx
@@ -1,10 +1,107 @@
-
+import "./PrivacyPolicy.css"
 const PrivacyPolicy = () => {
-    return (
-        <>
-          <h1>Privacy Policy of <span className="chinese-red">Chao kai Qi</span> </h1>  
-        </>
-    );
+  return (
+    <section id="privacy-policy">
+      <h1>Privacy Policy of <span className="chinese-red">Chao kai Qi</span> </h1>
+      <div>
+        <p className="font-bold">Effective Date: 1 January 2024</p>
+        <br />
+
+        At ChaoKaiQi we are committed to protecting the privacy and security of the personal information collected from users of our website. This Privacy Policy outlines how we collect, use, disclose, and safeguard your information when you use our website.
+      </div>
+
+      <div>
+        <img src="" alt="" />
+
+        <div>
+          <h2>Information We Collect</h2>
+          <ul>We collect personal information provided by users when they use our website, including but not limited to:
+            <li>Name</li>
+            <li>Company Name</li>
+            <li>Email Address</li>
+            <li>Phone Number</li>
+            <li>Address</li>
+            <li>Product Preferences</li>
+            <li>Messages submitted through forms</li>
+          </ul>
+        </div>
+      </div>
+
+      <div>
+        <img src="" alt="" />
+
+        <div>
+          <h2>How We Use Your Information</h2>
+          <ul>We use the collected information for the following purposes:
+            <li>To process and respond to inquiries and requests submitted through our website.</li>
+            <li>To communicate with users regarding their product preferences and related services.</li>
+            <li>To improve our website and services based on user feedback.</li>
+          </ul>
+        </div>
+      </div>
+
+      <div>
+        <img src="" alt="" />
+
+        <div>
+          <h2>Information Sharing</h2>
+
+          <p>We do not sell, trade, or otherwise transfer your personally identifiable information to outside parties. This does not include trusted third parties who assist us in operating our website or conducting our business, provided that these parties agree to keep this information confidential.</p>
+        </div>
+      </div>
+
+      <div>
+        <img src="" alt="" />
+
+        <div>
+          <h2>Data Security</h2>
+
+          <p>We implement a variety of security measures to maintain the safety of your personal information when you submit a request or access your personal information.</p>
+        </div>
+      </div>
+
+      <div>
+        <img src="" alt="" />
+
+        <div>
+          <h2>Retention of Information</h2>
+
+          <p>We retain the information collected for as long as necessary to fulfill the purposes outlined in this Privacy Policy, unless a longer retention period is required or permitted by law.</p>
+        </div>
+      </div>
+
+      <div>
+        <img src="" alt="" />
+
+        <div>
+          <h2>Third-Party Links</h2>
+
+          <p>Our website may contain links to third-party websites. We are not responsible for the privacy practices or the content of these websites.</p>
+        </div>
+      </div>
+
+      <div>
+        <img src="" alt="" />
+
+        <div>
+          <h2>Changes to the Privacy Policy</h2>
+
+          <p>We reserve the right to update this Privacy Policy at any time. The effective date will be posted at the beginning of this policy.</p>
+        </div>
+      </div>
+
+      <div>
+        <img src="" alt="" />
+
+        <div>
+          <h2>Contact Information</h2>
+
+          <p>If you have any questions or concerns about our Privacy Policy, please contact us at [Your Contact Information].</p>
+        </div>
+      </div>
+
+    </section>
+  );
 };
 
 export default PrivacyPolicy;
