870443c526e765bb927ac7be9a83152de62435d3
Md Antor <69227191+Antor1155@users.noreply.github.com>
2024-01-08 18:34:46 +0600

    unused img tag from privacy POlicy.jsx removed

--- src/Pages/AboutUs/AboutUs.jsx
+++ src/Pages/AboutUs/AboutUs.jsx
@@ -5,7 +5,15 @@ const AboutUs = () => {
             <h1 className="company-font text-center mb-gapping-tape">About <br />ChaoKaiQi</h1>
 
             <section>
-                hello
+                <div>
+                    <h2 className="mb-16">
+                        <span className="chinese-red">Chao Kai Qi :</span> <br />
+                        A globally trusted name for <br />
+                        Wholesale phone case-cover supplier
+                    </h2>
+
+                    <p>Welcome to Chao Kai Qi Technology Co. Ltd, your trusted global wholesaler for premium phone cases and quality covers. Based in Shenzen, China, we pride ourselves on delivering innovative, durable, and stylish products to phone case sellers worldwide. Our mission is simple: to redefine the market with a perfect blend of quality, affordability, and unwavering trust. At Chao Kai Qi, we are not just providing phone accessories; we are shaping the future of the industry. Explore our collection and experience the difference. For inquiries, contact us at <a href="mailto:Chaokaiqi@outlook.com">Chaokaiqi@outlook.com</a> or <a href="tel:+86 13798587275">+86 13798587275</a> (WhatsApp). Let&#39;s redefine phone case excellence together.</p>
+                </div>
             </section>
         </>
     );
