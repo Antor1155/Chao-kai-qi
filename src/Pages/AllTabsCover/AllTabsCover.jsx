af2c4e2c4d6dec7a24fe33c10673c4b41bdbd4c0
Md Antor <69227191+Antor1155@users.noreply.github.com>
2024-01-14 04:01:23 +0600

    all product page for desktop version done

--- src/Pages/AllTabsCover/AllTabsCover.jsx
+++ src/Pages/AllTabsCover/AllTabsCover.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import SingleProduct from "../../components/SingleProduct/SingleProduct";
+import { motion } from "framer-motion"
 
 import "./AllTabsCover.css"
 
@@ -11,7 +12,7 @@ const AllTabsCover = () => {
 
     return (
         <>
-            <h1 className="mb-gapping-tape align-center">Ipad air 2020 tablet covers</h1>
+            <h1 className="mb-gapping-tape align-center"> <span className="chinese-red">Ipad air 2020</span> tablet covers</h1>
 
             <section className="all-products-section">
                 <div className="filter-catagories">
@@ -42,20 +43,82 @@ const AllTabsCover = () => {
 
                         <p>
                             <input type="checkbox" name="ipad 2022" id="ipad 2020" />
-                            <label htmlFor="ipad 2022"> Ipad 2022</label>
+                            <label htmlFor="ipad 2022"> S8 ultra</label>
                         </p>
                         <p>
                             <input type="checkbox" name="ipad 2022" id="ipad 2020" />
-                            <label htmlFor="ipad 2022"> Ipad 2022</label>
+                            <label htmlFor="ipad 2022"> S6 ultra</label>
                         </p>
                         <p>
                             <input type="checkbox" name="ipad 2022" id="ipad 2020" />
-                            <label htmlFor="ipad 2022"> Ipad 2022</label>
+                            <label htmlFor="ipad 2022">Tab A8</label>
                         </p>
                         <p>
                             <input type="checkbox" name="ipad 2022" id="ipad 2020" />
-                            <label htmlFor="ipad 2022"> Ipad 2022</label>
+                            <label htmlFor="ipad 2022"> S9 plus</label>
                         </p>
+                    </div>
+
+
+                    <div className="single-filter">
+                        <h2 className="company-font font-bold">Cover style</h2>
+
+                        <p>
+                            <input type="checkbox" name="ipad 2022" id="ipad 2020" />
+                            <label htmlFor="ipad 2022"> Magnetic</label>
+                        </p>
+                        <p>
+                            <input type="checkbox" name="ipad 2022" id="ipad 2020" />
+                            <label htmlFor="ipad 2022"> Striped</label>
+                        </p>
+                        <p>
+                            <input type="checkbox" name="ipad 2022" id="ipad 2020" />
+                            <label htmlFor="ipad 2022"> Special</label>
+                        </p>
+                        <p>
+                            <input type="checkbox" name="ipad 2022" id="ipad 2020" />
+                            <label htmlFor="ipad 2022"> Glass special</label>
+                        </p>
+                    </div>
+
+                    <div className="single-filter">
+                        <h2 className="company-font font-bold">Colors</h2>
+
+                        <div className="color-buttons">
+                            <motion.button 
+                                whileTap={{ scale: 0.9 }}
+                                whileHover={{scale:1.1}}
+                                style={{ background: "red" }}
+                            ></motion.button>
+                            <motion.button 
+                                whileTap={{ scale: 0.9 }}
+                                whileHover={{scale:1.1}}
+                                style={{ background: "green" }}
+                            ></motion.button>
+                            <motion.button 
+                                whileTap={{ scale: 0.9 }}
+                                whileHover={{scale:1.1}}
+                                style={{ background: "yellow" }}
+                            ></motion.button>
+                            <motion.button 
+                                whileTap={{ scale: 0.9 }}
+                                whileHover={{scale:1.1}}
+                                style={{ background: "black" }}
+                            ></motion.button>
+                            <motion.button 
+                                whileTap={{ scale: 0.9 }}
+                                whileHover={{scale:1.1}}
+                                style={{ background: "cyan" }}
+                            ></motion.button>
+                            <motion.button 
+                                whileTap={{ scale: 0.9 }}
+                                whileHover={{scale:1.1}}
+                                style={{ background: "white" }}
+                            ></motion.button>
+
+
+
+                        </div>
                     </div>
 
                 </div>
