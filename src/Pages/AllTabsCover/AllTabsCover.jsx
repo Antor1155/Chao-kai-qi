53caafdffc8c0bc478385c7679a33a47861db304
Md Antor <69227191+Antor1155@users.noreply.github.com>
2024-01-14 05:08:30 +0600

    all product page mobile version completed

--- src/Pages/AllTabsCover/AllTabsCover.jsx
+++ src/Pages/AllTabsCover/AllTabsCover.jsx
@@ -4,18 +4,31 @@ import { motion } from "framer-motion"
 
 import "./AllTabsCover.css"
 
+import filter from "../../assets/logos/Filter.svg"
+
 const AllTabsCover = () => {
     const [products, setProducts] = useState([])
+    const [mobileFilterVisible, setMobileFilterVisible] = useState(false)
+
     useEffect(() => {
         setProducts([5, 2, 5, 33, 22, 55, 22, 11, 44])
     }, [])
 
     return (
-        <>
-            <h1 className="mb-gapping-tape align-center"> <span className="chinese-red">Ipad air 2020</span> tablet covers</h1>
+        <section className="all-products-page">
+            <h1 className="align-center"> <span className="chinese-red">Ipad air 2020</span> tablet covers</h1>
+
+            <motion.button
+                className="filter-button"
+                whileTap={{ scale: 0.9 }}
+                onClick={() => setMobileFilterVisible(true)}
+            >
+                <img src={filter} alt="filter button" />
+            </motion.button>
 
             <section className="all-products-section">
-                <div className="filter-catagories">
+
+                <div className={`filter-catagories  ${mobileFilterVisible ? "visible" : ""} `} id="filter-catagories">
 
                     <div className="single-filter">
                         <h2 >Iphone</h2>
@@ -35,6 +48,27 @@ const AllTabsCover = () => {
                         <p>
                             <input type="checkbox" name="ipad 2022" id="ipad 2020" />
                             <label htmlFor="ipad 2022"> Ipad 2022</label>
+                        </p>
+                    </div>
+
+                    <div className="single-filter">
+                        <h2 >Samsung</h2>
+
+                        <p>
+                            <input type="checkbox" name="ipad 2022" id="ipad 2020" />
+                            <label htmlFor="ipad 2022"> S8 ultra</label>
+                        </p>
+                        <p>
+                            <input type="checkbox" name="ipad 2022" id="ipad 2020" />
+                            <label htmlFor="ipad 2022"> S6 ultra</label>
+                        </p>
+                        <p>
+                            <input type="checkbox" name="ipad 2022" id="ipad 2020" />
+                            <label htmlFor="ipad 2022">Tab A8</label>
+                        </p>
+                        <p>
+                            <input type="checkbox" name="ipad 2022" id="ipad 2020" />
+                            <label htmlFor="ipad 2022"> S9 plus</label>
                         </p>
                     </div>
 
@@ -85,40 +119,49 @@ const AllTabsCover = () => {
                         <h2 className="company-font font-bold">Colors</h2>
 
                         <div className="color-buttons">
-                            <motion.button 
+                            <motion.button
                                 whileTap={{ scale: 0.9 }}
-                                whileHover={{scale:1.1}}
+                                whileHover={{ scale: 1.1 }}
                                 style={{ background: "red" }}
                             ></motion.button>
-                            <motion.button 
+                            <motion.button
                                 whileTap={{ scale: 0.9 }}
-                                whileHover={{scale:1.1}}
+                                whileHover={{ scale: 1.1 }}
                                 style={{ background: "green" }}
                             ></motion.button>
-                            <motion.button 
+                            <motion.button
                                 whileTap={{ scale: 0.9 }}
-                                whileHover={{scale:1.1}}
+                                whileHover={{ scale: 1.1 }}
                                 style={{ background: "yellow" }}
                             ></motion.button>
-                            <motion.button 
+                            <motion.button
                                 whileTap={{ scale: 0.9 }}
-                                whileHover={{scale:1.1}}
+                                whileHover={{ scale: 1.1 }}
                                 style={{ background: "black" }}
                             ></motion.button>
-                            <motion.button 
+                            <motion.button
                                 whileTap={{ scale: 0.9 }}
-                                whileHover={{scale:1.1}}
+                                whileHover={{ scale: 1.1 }}
                                 style={{ background: "cyan" }}
                             ></motion.button>
-                            <motion.button 
+                            <motion.button
                                 whileTap={{ scale: 0.9 }}
-                                whileHover={{scale:1.1}}
+                                whileHover={{ scale: 1.1 }}
                                 style={{ background: "white" }}
                             ></motion.button>
 
 
 
                         </div>
+                    </div>
+
+                    <div id="mobile-filter-apply">
+                        <motion.button
+                            whileTap={{ scale: 0.9 }}
+                            onClick={() => setMobileFilterVisible(false)}
+                        >
+                            APPLY
+                        </motion.button>
                     </div>
 
                 </div>
@@ -129,7 +172,7 @@ const AllTabsCover = () => {
                 </div>
             </section>
 
-        </>
+        </section>
     );
 };
 
