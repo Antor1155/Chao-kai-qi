2ebe096f64801dd59f68f70efc62581f90ea50c5
Md Antor <69227191+Antor1155@users.noreply.github.com>
2024-01-14 03:37:57 +0600

    initial setup of all product page done

--- src/Pages/AllTabsCover/AllTabsCover.jsx
+++ src/Pages/AllTabsCover/AllTabsCover.jsx
@@ -1,9 +1,72 @@
+import { useEffect, useState } from "react";
+import SingleProduct from "../../components/SingleProduct/SingleProduct";
+
+import "./AllTabsCover.css"
 
 const AllTabsCover = () => {
+    const [products, setProducts] = useState([])
+    useEffect(() => {
+        setProducts([5, 2, 5, 33, 22, 55, 22, 11, 44])
+    }, [])
+
     return (
-        <div>
-            this is all tabs covers
-        </div>
+        <>
+            <h1 className="mb-gapping-tape align-center">Ipad air 2020 tablet covers</h1>
+
+            <section className="all-products-section">
+                <div className="filter-catagories">
+
+                    <div className="single-filter">
+                        <h2 >Iphone</h2>
+
+                        <p>
+                            <input type="checkbox" name="ipad 2022" id="ipad 2020" />
+                            <label htmlFor="ipad 2022"> Ipad 2022</label>
+                        </p>
+                        <p>
+                            <input type="checkbox" name="ipad 2022" id="ipad 2020" />
+                            <label htmlFor="ipad 2022"> Ipad 2022</label>
+                        </p>
+                        <p>
+                            <input type="checkbox" name="ipad 2022" id="ipad 2020" />
+                            <label htmlFor="ipad 2022"> Ipad 2022</label>
+                        </p>
+                        <p>
+                            <input type="checkbox" name="ipad 2022" id="ipad 2020" />
+                            <label htmlFor="ipad 2022"> Ipad 2022</label>
+                        </p>
+                    </div>
+
+                    <div className="single-filter">
+                        <h2 >Samsung</h2>
+
+                        <p>
+                            <input type="checkbox" name="ipad 2022" id="ipad 2020" />
+                            <label htmlFor="ipad 2022"> Ipad 2022</label>
+                        </p>
+                        <p>
+                            <input type="checkbox" name="ipad 2022" id="ipad 2020" />
+                            <label htmlFor="ipad 2022"> Ipad 2022</label>
+                        </p>
+                        <p>
+                            <input type="checkbox" name="ipad 2022" id="ipad 2020" />
+                            <label htmlFor="ipad 2022"> Ipad 2022</label>
+                        </p>
+                        <p>
+                            <input type="checkbox" name="ipad 2022" id="ipad 2020" />
+                            <label htmlFor="ipad 2022"> Ipad 2022</label>
+                        </p>
+                    </div>
+
+                </div>
+
+                <div className="result-products">
+                    {products.map((product, ind) => <SingleProduct key={ind} product={product}></SingleProduct>)}
+
+                </div>
+            </section>
+
+        </>
     );
 };
 
