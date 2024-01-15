c53212cdada042bf1a9df90f36dc393e7e647c46
Md Antor <69227191+Antor1155@users.noreply.github.com>
2024-01-15 06:40:16 +0600

    singleProductPage upto order add by form submit complated, index.css updated to add button collor and button hover color in :root

--- src/Pages/SingleProductPage/SingleProductPage.jsx
+++ src/Pages/SingleProductPage/SingleProductPage.jsx
@@ -1,6 +1,9 @@
 import { useEffect, useState } from "react";
 import "./SingleProductPage.css"
 import Slider from "react-slick";
+import { motion } from "framer-motion"
+
+import star from "../../assets/logos/Star.svg"
 
 const schemaData = {
     ProductName: "ipad mini 6",
@@ -9,21 +12,24 @@ const schemaData = {
     pricePerUnit: 2.6,
     productSize: "200 * 150 * 13 cm",
     productGrossWeight: "198 g",
-    imageArray: ["/ProductImages/example-type/IPAD1.jpg", "/ProductImages/example-type/IPAD2.jpg", "/ProductImages/example-type/IPAD3.jpg","/ProductImages/example-type/IPAD1.jpg"],
+    imageArray: ["/ProductImages/example-type/IPAD1.jpg", "/ProductImages/example-type/IPAD2.jpg", "/ProductImages/example-type/IPAD3.jpg", "/ProductImages/example-type/IPAD1.jpg"],
 
     mainImage: "/ProductImages/example-type/IPAD1.jpg",
     colors: {
-        red: {
-            colorValue: "#ff0000",
+        roseGold: {
+            name: "rose gold",
+            colorValue: "#B78E89",
             imgLink: "/ProductImages/example-type/IPAD1.jpg"
         },
-        green: {
-            colorValue: "#097e7b",
-            imgLink: "/ProductImages/example-type/IPAD1.jpg"
+        navy: {
+            name: "navy",
+            colorValue: "#3B3B5F",
+            imgLink: "/ProductImages/example-type/IPAD2.jpg"
         },
-        pink: {
-            colorValue: "#f88cae",
-            imgLink: "/ProductImages/example-type/IPAD1.jpg"
+        black: {
+            name: "black",
+            colorValue: "#303030",
+            imgLink: "/ProductImages/example-type/IPAD3.jpg"
         }
     },
 
@@ -33,11 +39,19 @@ const SingleProductPage = () => {
     const [product, setProduct] = useState({})
     const [mainImage, setMainImage] = useState("")
 
-    console.log(product)
+    const [orderAmount, setOrderAmount] = useState()
+
+    const [selectedColor, setSelectedColor] = useState({
+        name: "all",
+        colorValue: "",
+        imgLink: ""
+    })
+
 
     useEffect(() => {
         setProduct(schemaData)
         setMainImage(schemaData.mainImage)
+        setOrderAmount(schemaData.minimOrderQuantity)
     }, [])
 
     const slickSettings = {
@@ -47,6 +61,33 @@ const SingleProductPage = () => {
         slidesToShow: 3,
         slidesToScroll: 1,
     }
+
+    const handleSelectedColor = (val) => {
+        if (val.name === selectedColor.name) {
+            setSelectedColor({
+                name: "all",
+                colorValue: "",
+                imgLink: ""
+            })
+            setMainImage(product?.mainImage)
+        } else {
+            setSelectedColor(val)
+            setMainImage(val?.imgLink)
+        }
+    }
+
+    const handleAddToCard = (e) => {
+        e.preventDefault()
+        const orderQuantity = e.target.orderQuantity.value 
+
+        if(orderQuantity){
+            setOrderAmount(parseInt(orderQuantity))
+        }
+        
+        e.target.reset()
+    }
+
+    console.log(orderAmount, typeof(orderAmount))
 
     return (
         <>
@@ -60,14 +101,79 @@ const SingleProductPage = () => {
                         <Slider {...slickSettings}>
 
                             {product?.imageArray?.map((img, ind) =>
-                                <img key={ind} src={img} alt={product?.ProductName} onClick={()=>setMainImage(img)}/>
+                                <img key={ind} src={img} alt={product?.ProductName} onClick={() => setMainImage(img)} />
                             )}
                         </Slider>
                     </div>
                 </div>
 
                 <div className="content-section">
-                    product description
+                    <h1 className="mb-16"><span className="chinese-red">{product?.ProductName}</span> cover</h1>
+
+                    <h2 className="font-24 mb-4">$ {product?.pricePerUnit} USD <span className="title">per unit</span></h2>
+
+                    <p className="review-stars mb-16">
+                        <img src={star} alt="client review star" />
+                        <img src={star} alt="client review star" />
+                        <img src={star} alt="client review star" />
+                        <img src={star} alt="client review star" />
+                        <img src={star} alt="client review star" />
+                    </p>
+
+                    <h2 className="font-24 mb-16"> <span className="title">Cover name</span> : {product?.coverName}</h2>
+
+                    <h2 className="font-24 mb-16"> <span className="title">Product size</span> : {product?.productSize}</h2>
+
+                    <h2 className="font-24 mb-16"> <span className="title">Gross weight</span> : {product?.productGrossWeight}</h2>
+
+                    <h2 className="font-24 mb-32"> <span className="title">Minimum order quantity</span> : {product?.minimOrderQuantity} units</h2>
+
+                    <h2 className="company-font font-bold font-32 mb-8">Colors</h2>
+
+                    <div className="color-buttons mb-32">
+                        {
+                            product?.colors ?
+
+                                Object.values(product?.colors).map((val, ind) =>
+                                    <motion.button
+                                        key={ind}
+                                        whileTap={{ scale: 0.9 }}
+                                        whileHover={{ scale: 1.1 }}
+                                        style={
+                                            {
+                                                background: val?.colorValue,
+                                                border: (val.name === selectedColor.name) ? "5px solid white" : "1px solid",
+                                            }
+                                        }
+
+                                        onClick={() => handleSelectedColor(val)}
+                                    ></motion.button>
+                                )
+                                :
+                                ""
+                        }
+                    </div>
+
+                    <form onSubmit={handleAddToCard} id="order-amount-form">
+                        <label className="font-24" htmlFor="orderQuantity">
+                            <span className="title">
+                                Order amount
+                            </span>
+                        </label>
+
+                        <input type="number" name="orderQuantity" id="orderQuantity" min={product?.minimOrderQuantity} placeholder={"min amount : " + product?.minimOrderQuantity}  />
+
+                        <motion.button
+                            className="fw-button"
+                            type="submit"
+                            whileTap={{scale:0.95}}
+                        >
+                            ADD TO CART
+                        </motion.button>
+                    </form>
+
+
+
                 </div>
             </section>
         </>
