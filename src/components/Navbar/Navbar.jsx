8f07f8e63e6f4ad9d74af616d2aaf271117d6777
Md Antor <69227191+Antor1155@users.noreply.github.com>
2024-01-21 10:25:36 +0600

    catagory button and on hover, go catagories section working fine

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,12 +1,20 @@
 import { Link } from "react-router-dom";
-import { motion } from "framer-motion"
+import { AnimatePresence, motion } from "framer-motion"
 import languageButton from "../../assets/logos/language-options.svg";
 import cart from "../../assets/logos/cart.svg"
 import menu from "../../assets/logos/Menu.svg"
 import cancel from "../../assets/logos/Cancel.svg"
 import "./Navbar.css"
+import { useEffect, useState } from "react";
 
 const Navbar = () => {
+    const [isCatagoryOpen, setCatagoryOpen] = useState(true)
+
+    useEffect(()=>{
+
+    }, [])
+
+
     const container = {
         hidden: { opacity: 0 },
         show: {
@@ -22,7 +30,7 @@ const Navbar = () => {
         show: { opacity: 1 },
     }
 
-    const handleNavMenues = ()=>{
+    const handleNavMenues = () => {
         document.getElementById("nav-menues").classList.toggle("visible")
     }
 
@@ -43,23 +51,32 @@ const Navbar = () => {
                     id="nav-menues"
                     onClick={handleNavMenues}
                 >
-                    <motion.button whileTap={{scale: .7}} id="cancel">
+                    <motion.button whileTap={{ scale: .7 }} id="cancel">
                         <img src={cancel} alt="hide nav bar" />
                     </motion.button>
 
-                    <motion.span variants={item} whileTap={{scale: .7}}><Link to="/">HOME</Link></motion.span>
-                    <motion.span variants={item} whileTap={{scale: .7}}><Link to="/">CATAGORY</Link></motion.span>
-                    <motion.span variants={item} whileTap={{scale: .7}}><Link to="/all-wholesale-tablet-covers/all">ALL PRODUCTS</Link></motion.span>
-                    <motion.span variants={item} whileTap={{scale: .7}}><Link to="/about-us">ABOUT US</Link></motion.span>
-                    <motion.span variants={item} whileTap={{scale: .7}}><Link to="/contact-us">CONTACT US</Link></motion.span>
+                    <motion.span variants={item} whileTap={{ scale: .7 }}><Link to="/">HOME</Link></motion.span>
 
-                    <motion.span variants={item} whileTap={{scale: .7}}>
-                        <Link to="/">
-                            <img src={languageButton} alt="language changing button" className="logo" />
-                        </Link>
+                    <motion.span
+                        variants={item}
+                        whileTap={{ scale: .7 }}
+                        style={{ cursor: "pointer" }}
+                        onMouseEnter={() => setCatagoryOpen(true)}
+                        onMouseLeave={() => setCatagoryOpen(false)}
+                        whileHover={{color:"#C90A1E"}}
+                    >
+                        CATAGORY
                     </motion.span>
 
-                    <motion.span variants={item} whileTap={{scale: .7}}>
+                    <motion.span variants={item} whileTap={{ scale: .7 }}><Link to="/all-wholesale-tablet-covers/all">ALL PRODUCTS</Link></motion.span>
+                    <motion.span variants={item} whileTap={{ scale: .7 }}><Link to="/about-us">ABOUT US</Link></motion.span>
+                    <motion.span variants={item} whileTap={{ scale: .7 }}><Link to="/contact-us">CONTACT US</Link></motion.span>
+
+                    <motion.span variants={item} whileTap={{ scale: .7 }}>
+                        <img src={languageButton} alt="language changing button" className="logo" />
+                    </motion.span>
+
+                    <motion.span variants={item} whileTap={{ scale: .7 }}>
                         <Link to="/cart">
                             <img src={cart} alt="cart page of products" className="logo" />
                         </Link>
@@ -68,6 +85,33 @@ const Navbar = () => {
                 </motion.div>
 
             </nav>
+
+            <AnimatePresence>
+                {
+                    isCatagoryOpen &&
+                    <motion.div
+                        onMouseEnter={()=>setCatagoryOpen(true)}
+                        onMouseLeave={()=>setCatagoryOpen(false)}
+
+                        id="catagories"
+                        initial={{ opacity: 0 }}
+                        animate={{ opacity: 1 }}
+                        exit={{ opacity: 0 }}
+                        transition={{delay:0.1}}
+                    >
+                        <p>hello world</p>
+                        <p>hello world</p>
+                        <p>hello world</p>
+                        <p>hello world</p>
+                        <p>hello world</p>
+                        <p>hello world</p>
+                        <p>hello world</p>
+                        <p>hello world</p>
+                        <p>hello world</p>
+                        <p>hello world</p>
+                    </motion.div>
+                }
+            </AnimatePresence>
         </header>
 
     );
