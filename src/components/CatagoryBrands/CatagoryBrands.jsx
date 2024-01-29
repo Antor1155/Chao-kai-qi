2905c962b803b86cd2a15bcf0f069407a6493787
Md Antor <69227191+Antor1155@users.noreply.github.com>
2024-01-30 00:14:34 +0600

    CatagoryBrands.jsx rearranged and made position fixed

--- src/components/CatagoryBrands/CatagoryBrands.jsx
+++ src/components/CatagoryBrands/CatagoryBrands.jsx
@@ -13,6 +13,8 @@ const CatagoryBrands = ({ isCatagoryOpen, setCatagoryOpen }) => {
 
     const [brands, setBrands] = useState([])
 
+    console.log(brands)
+
     useEffect(() => {
         instance.get("/brands")
             .then(data => {
@@ -39,28 +41,164 @@ const CatagoryBrands = ({ isCatagoryOpen, setCatagoryOpen }) => {
                     >
 
                         <motion.button className="mobile-view"
-                            onClick={()=>setCatagoryOpen(false)}
-                            whileTap={{scale:0.9}}
+                            onClick={() => setCatagoryOpen(false)}
+                            whileTap={{ scale: 0.9 }}
                         >
                             <img src={cancel} alt="cancle button" />
                         </motion.button>
 
-                        {brands.map(brand => (
-                            <div key={brand._id} className="single-brans">
-                                <h2 className="mb-16 chinese-red">{brand._id}</h2>
+                        {
+                            brands.length ?
 
-                                {brand?.products.map(model => (
-                                    <Link key={model} to={`/all-wholesale-tablet-covers/${model}`}>
-                                        <motion.span
-                                            whileHover={{color:"#25CBE1"}}
-                                            onClick={()=>setCatagoryOpen(false)}
-                                        >
-                                            {model}
-                                        </motion.span>
-                                    </Link>
-                                ))}
-                            </div>
-                        ))}
+                                <>
+                                    <div className="single-brans">
+                                        <h2 className="mb-16 chinese-red">{brands[0]._id}</h2>
+
+                                        {brands[0].products.map(model => (
+                                            <Link key={model} to={`/all-wholesale-tablet-covers/${model}`}>
+                                                <motion.span
+                                                    whileHover={{ color: "#25CBE1" }}
+                                                    onClick={() => setCatagoryOpen(false)}
+                                                >
+                                                    {model}
+                                                </motion.span>
+                                            </Link>
+                                        ))}
+                                    </div>
+
+                                    <div className="single-brans">
+                                        <h2 className="mb-16 chinese-red">{brands[6]._id}</h2>
+
+                                        {brands[6].products.map(model => (
+                                            <Link key={model} to={`/all-wholesale-tablet-covers/${model}`}>
+                                                <motion.span
+                                                    whileHover={{ color: "#25CBE1" }}
+                                                    onClick={() => setCatagoryOpen(false)}
+                                                >
+                                                    {model}
+                                                </motion.span>
+                                            </Link>
+                                        ))}
+                                    </div>
+
+                                    <div>
+                                        <div className="single-brans">
+                                            <h2 className="mb-16 chinese-red">{brands[1]._id}</h2>
+
+                                            {brands[1].products.map(model => (
+                                                <Link key={model} to={`/all-wholesale-tablet-covers/${model}`}>
+                                                    <motion.span
+                                                        whileHover={{ color: "#25CBE1" }}
+                                                        onClick={() => setCatagoryOpen(false)}
+                                                    >
+                                                        {model}
+                                                    </motion.span>
+                                                </Link>
+                                            ))}
+                                        </div>
+
+                                        <div className="single-brans">
+                                            <h2 className="mb-16 chinese-red">{brands[2]._id}</h2>
+
+                                            {brands[2].products.map(model => (
+                                                <Link key={model} to={`/all-wholesale-tablet-covers/${model}`}>
+                                                    <motion.span
+                                                        whileHover={{ color: "#25CBE1" }}
+                                                        onClick={() => setCatagoryOpen(false)}
+                                                    >
+                                                        {model}
+                                                    </motion.span>
+                                                </Link>
+                                            ))}
+                                        </div>
+                                    </div>
+
+                                    <div>
+                                        <div className="single-brans">
+                                            <h2 className="mb-16 chinese-red">{brands[5]._id}</h2>
+
+                                            {brands[5].products.map(model => (
+                                                <Link key={model} to={`/all-wholesale-tablet-covers/${model}`}>
+                                                    <motion.span
+                                                        whileHover={{ color: "#25CBE1" }}
+                                                        onClick={() => setCatagoryOpen(false)}
+                                                    >
+                                                        {model}
+                                                    </motion.span>
+                                                </Link>
+                                            ))}
+                                        </div>
+
+                                        <div className="single-brans">
+                                            <h2 className="mb-16 chinese-red">{brands[8]._id}</h2>
+
+                                            {brands[8].products.map(model => (
+                                                <Link key={model} to={`/all-wholesale-tablet-covers/${model}`}>
+                                                    <motion.span
+                                                        whileHover={{ color: "#25CBE1" }}
+                                                        onClick={() => setCatagoryOpen(false)}
+                                                    >
+                                                        {model}
+                                                    </motion.span>
+                                                </Link>
+                                            ))}
+                                        </div>
+                                    </div>
+
+                                    <div>
+                                        <div className="single-brans">
+                                            <h2 className="mb-16 chinese-red">{brands[3]._id}</h2>
+
+                                            {brands[3].products.map(model => (
+                                                <Link key={model} to={`/all-wholesale-tablet-covers/${model}`}>
+                                                    <motion.span
+                                                        whileHover={{ color: "#25CBE1" }}
+                                                        onClick={() => setCatagoryOpen(false)}
+                                                    >
+                                                        {model}
+                                                    </motion.span>
+                                                </Link>
+                                            ))}
+                                        </div>
+
+                                        <div className="single-brans">
+                                            <h2 className="mb-16 chinese-red">{brands[4]._id}</h2>
+
+                                            {brands[4].products.map(model => (
+                                                <Link key={model} to={`/all-wholesale-tablet-covers/${model}`}>
+                                                    <motion.span
+                                                        whileHover={{ color: "#25CBE1" }}
+                                                        onClick={() => setCatagoryOpen(false)}
+                                                    >
+                                                        {model}
+                                                    </motion.span>
+                                                </Link>
+                                            ))}
+                                        </div>
+
+                                        <div className="single-brans">
+                                            <h2 className="mb-16 chinese-red">{brands[7]._id}</h2>
+
+                                            {brands[7].products.map(model => (
+                                                <Link key={model} to={`/all-wholesale-tablet-covers/${model}`}>
+                                                    <motion.span
+                                                        whileHover={{ color: "#25CBE1" }}
+                                                        onClick={() => setCatagoryOpen(false)}
+                                                    >
+                                                        {model}
+                                                    </motion.span>
+                                                </Link>
+                                            ))}
+                                        </div>
+                                    </div>
+
+
+
+                                </>
+                                :
+                                ""
+
+                        }
 
                     </motion.div>
                 }
