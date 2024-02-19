af61abfdeb6121996698b923059b996a10a77e15
Md Antor <69227191+Antor1155@users.noreply.github.com>
2024-02-19 14:03:47 +0600

    singleProduct.jsx added to en.json

--- src/components/SingleProduct/SingleProduct.jsx
+++ src/components/SingleProduct/SingleProduct.jsx
@@ -1,9 +1,11 @@
 import "./SingleProduct.css"
 import { Link } from "react-router-dom";
+import { useTranslation } from "react-i18next";
 
 
 // eslint-disable-next-line react/prop-types
 const SingleProduct = ({ product }) => {
+    const { t } = useTranslation()
 
     // eslint-disable-next-line react/prop-types
     const { _id, productName, coverName, minimOrderQuantity, pricePerUnit, mainImage, colors } = product
@@ -17,18 +19,18 @@ const SingleProduct = ({ product }) => {
                 <div className="image-container">
                     <img src={mainImage + ".jpg"} alt="phone case and cover of wholesale supplier" loading="lazy" />
                 </div>
-                <h3 className="font-16">{productName} Cover</h3>
-                <p className="font-14 mb-8">{coverName}</p>
+                <h3 className="font-16">{productName} {t("singleProduct.cover")}</h3>
+                <p className="font-14 mb-8">{t(`coverName.${coverName}`)}</p>
 
                 <div className="color-options mb-8">
                     {Object.values(colors)?.map(color => (
                         <div key={color.colorValue} style={{ backgroundColor: color.colorValue }}></div>))}
                 </div>
 
-                <p className="font-14">Minimum Order quantity: {minimOrderQuantity} units</p>
-                <p className="font-14 mb-16">Price / unit : {pricePerUnit} USD</p>
+                <p className="font-14">{t("singleProduct.minimum")} : {minimOrderQuantity} {t("singleProduct.units")}</p>
+                <p className="font-14 mb-16">{t("singleProduct.price")} : {pricePerUnit} USD</p>
 
-                <button className="fw-button">SEE THE COVER</button>
+                <button className="fw-button">{t("singleProduct.see")}</button>
 
             </div>
         </Link>
