2dffbf573bc4aa1db1eb86bb7ce4c5a8e7533e31
Md Antor <69227191+Antor1155@users.noreply.github.com>
2024-02-19 11:51:05 +0600

    return policy and shipping policy added to en.json

--- src/Pages/ReturnPolicy/ReturnPolicy.jsx
+++ src/Pages/ReturnPolicy/ReturnPolicy.jsx
@@ -1,32 +1,25 @@
 import exImg from "../../assets/companyImage/companyImg15.jpg"
 
 import "./ReturnPolicy.css"
+import { useTranslation } from "react-i18next";
 
 const ReturnPolicy = () => {
+    const {t} = useTranslation()
     return (
         <section className="return-policy">
-            <h1>Return And Refund Policy of ChaoKaiQi</h1>
+            <h1>{t("return.title")}</h1>
             <img className="main-img" src={exImg} alt="happy customer returning product to ChaoKaiQi" />
 
-            <h2>Returning Normal Products</h2>
-            <p>Please be advised that shipping fee and express shipping fee are non-refundable. Any
-                discounts that were applied with a minimum purchase requirement may not be
-                applicable after a removal of product(s) in an order. For discounts that are applicable on
-                second/subsequent item, the discount will be revoked on partial return of the order, and
-                the unreturned item will be charged in original price. Shipping fee may also be applied.
+            <h2>{t("return.returnN")}</h2>
+            <p>{t("return.returnP")}
             </p>
 
-            <h2>Returning Customized Products</h2>
-            <p>Customized products are your unique creative expressions. Returns on customized
-                item(s) for reasons other than CHAOKAIQI error will be subject to a 50% processing fee
-                on the refund amount (after adjustment of discount/shipping, if required).
+            <h2>{t("return.returnC")}</h2>
+            <p>{t("return.returnCP")}
             </p>
 
-            <h2>Refund Policy</h2>
-            <p>Returns made will be refunded in your original form of payment. If the payment was
-                made using credit / debit card or bank account, there might be a lead time required by
-                PayPal (our credit card settlement service provider) to set up the credit transaction with
-                your bank.
+            <h2>{t("return.refund")}</h2>
+            <p>{t("return.refundP")}
             </p>
 
 
--- src/Pages/ShippingPolicy/ShippingPolicy.jsx
+++ src/Pages/ShippingPolicy/ShippingPolicy.jsx
@@ -2,35 +2,32 @@ import exImg from "../../assets/companyImage/companyImg15.jpg"
 import usaIcon from "../../assets/logos/Usa.svg"
 import globeIcon from "../../assets/logos/Globe.svg"
 
+import { useTranslation } from "react-i18next"
+
 import "./ShippingPolicy.css"
 
 const ShippingPolicy = () => {
+    const {t} = useTranslation()
+
     return (
         <section className="shipping-policy">
-            <h1>Shipping Policy of ChaoKaiQi</h1>
+            <h1>{t("shipping.title")}</h1>
             <img className="main-img" src={exImg} alt="Shipping policy image of ChaoKaiQi" />
 
             <div className="shipping-info">
                 <div>
                     <img src={usaIcon} alt="shipping to USA" />
-                    <h2>For US orders: <br />6-15 business days </h2>
+                    <h2>{t("shipping.forU")} <br />{t("shipping.bDays")} </h2>
                 </div>
                 <div>
                     <img src={globeIcon} alt="global shipping" />
-                    <h2>For non-US orders: <br />5-30 business days</h2>
+                    <h2>{t("shipping.forG")} <br />{t("shipping.bDG")}</h2>
                 </div>
             </div>
 
-            <small> <span>Note: </span> The delivery time will vary a lot from country to country. In short, the shipping time
-                alone could be around,</small>
+            <small> <span>{t("shipping.note")}</span> {t("shipping.noteP")}</small>
 
-            <p>Delivery time is not guaranteed for Standard Shipping as there are a lot of factors
-                affecting its speed, thus we would suggest Express Shipping if there is a date that you
-                would like to meet. The above are estimated time provided by the postal service and do
-                not include the processing time of the Customs in the destination country. In some
-                cases, the local Customs office of your country may require additional documents and
-                time to have your package cleared, and this may also cause further delay on the
-                delivery.</p>
+            <p>{t("shipping.lastP")}</p>
 
         </section>
     );
