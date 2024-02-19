bc90593226aa0aa38244e93113c9252f5c05de11
Md Antor <69227191+Antor1155@users.noreply.github.com>
2024-02-19 11:33:56 +0600

    payment method and private policy added to en.json

--- src/Pages/PaymentMethods/PaymentMethods.jsx
+++ src/Pages/PaymentMethods/PaymentMethods.jsx
@@ -1,37 +1,39 @@
 import exImg from "../../assets/companyImage/companyImg15.jpg"
 
 import "../ReturnPolicy/ReturnPolicy.css"
+import { useTranslation } from "react-i18next";
 
 const PaymentMethods = () => {
+    const {t} = useTranslation()
     return (
         <section className="return-policy">
-            <h1>Payment Methods for ChaoKaiQi Product Purchase</h1>
+            <h1>{t("paymentM.title")}</h1>
             <img className="main-img" src={exImg} alt="happy customer buying tablet cover from ChaoKaiQi" />
 
-            <h2>Major bank transfers:</h2>
-            <p>We accept transfers from most major banks directly to our Chinese bank account. This is a reliable and familiar option for many customers. Please refer to our website or contact us for details on our bank account information and any specific transfer instructions.
+            <h2>{t("paymentM.major")}</h2>
+            <p>{t("paymentM.majorP")}
             </p>
 
-            <h2>Alipay:</h2>
-            <p>This popular Chinese e-wallet platform is a fast and hassle-free way to pay online. If you have an Alipay account, you can simply select it as your payment method at checkout and complete the transaction within seconds.
+            <h2>{t("paymentM.alipay")}</h2>
+            <p>{t("paymentM.alipayP")}
             </p>
 
-            <h2>Wechat Pay:</h2>
-            <p>Another widely used e-wallet in China, Wechat Pay offers a convenient and secure way to pay directly from your Wechat app. Choose Wechat Pay at checkout and scan the QR code to finalize your purchase.
+            <h2>{t("paymentM.wechat")}</h2>
+            <p>{t("paymentM.wechatP")}
             </p>
 
-            <small>We understand that different customers have different preferences, and we want to make sure everyone has a convenient way to pay for their purchases on our website.</small>
+            <small>{t("paymentM.weUnders")}</small>
 
 
-            <h2 className="mt-16">Here are some additional benefits of using our payment methods:</h2>
+            <h2 className="mt-16">{t("paymentM.hereAre")}</h2>
             <ul style={{marginLeft:"1rem"}} className="mb-32">
-                <li>Security: All transactions are processed through secure servers and encrypted to protect your financial information.</li>
-                <li>Convenience: Choose the payment method that best suits your needs and complete your purchase in just a few clicks.</li>
-                <li>Speed: Bank transfers, Alipay, and Wechat Pay offer fast and efficient ways to send your payment, ensuring your order is processed quickly.</li>
+                <li>{t("paymentM.security")}</li>
+                <li>{t("paymentM.conve")}</li>
+                <li>{t("paymentM.speed")}</li>
             
             </ul>
 
-            <p>For further information pelase contact at <a style={{textDecoration:"underline"}} href="mailto:Chaokaiqi@outlook.com">Chaokaiqi@outlook.com</a></p>
+            <p>{t("paymentM.forF")} <a style={{textDecoration:"underline"}} href="mailto:Chaokaiqi@outlook.com">Chaokaiqi@outlook.com</a></p>
 
 
 
--- src/Pages/PrivacyPolicy/PrivacyPolicy.jsx
+++ src/Pages/PrivacyPolicy/PrivacyPolicy.jsx
@@ -1,26 +1,30 @@
 import "./PrivacyPolicy.css"
+import { useTranslation } from "react-i18next";
+
 const PrivacyPolicy = () => {
+  const {t} = useTranslation()
+
   return (
     <section id="privacy-policy">
-      <h1>Privacy Policy of <span className="chinese-red">Chao kai Qi</span> </h1>
+      <h1>{t("privacy.title")} <span className="chinese-red">{t("privacy.ckq")}</span> </h1>
       <div>
-        <p className="font-bold">Effective Date: 1 January 2024</p>
+        <p className="font-bold">{t("privacy.effectiveD")}</p>
         <br />
 
-        At ChaoKaiQi we are committed to protecting the privacy and security of the personal information collected from users of our website. This Privacy Policy outlines how we collect, use, disclose, and safeguard your information when you use our website.
+        {t("privacy.effectiveP")}
       </div>
 
       <div>
         <div>
-          <h2>Information We Collect</h2>
-          <ul>We collect personal information provided by users when they use our website, including but not limited to:
-            <li>Name</li>
-            <li>Company Name</li>
-            <li>Email Address</li>
-            <li>Phone Number</li>
-            <li>Address</li>
-            <li>Product Preferences</li>
-            <li>Messages submitted through forms</li>
+          <h2>{t("privacy.info")}</h2>
+          <ul>{t("privacy.weCollect")}
+            <li>{t("privacy.name")}</li>
+            <li>{t("privacy.company")}</li>
+            <li>{t("privacy.emailA")}</li>
+            <li>{t("privacy.phoneN")}</li>
+            <li>{t("privacy.address")}</li>
+            <li>{t("privacy.productP")}</li>
+            <li>{t("privacy.message")}</li>
           </ul>
         </div>
       </div>
@@ -28,11 +32,11 @@ const PrivacyPolicy = () => {
       <div>
 
         <div>
-          <h2>How We Use Your Information</h2>
-          <ul>We use the collected information for the following purposes:
-            <li>To process and respond to inquiries and requests submitted through our website.</li>
-            <li>To communicate with users regarding their product preferences and related services.</li>
-            <li>To improve our website and services based on user feedback.</li>
+          <h2>{t("privacy.howW")}</h2>
+          <ul>{t("privacy.weUse")}
+            <li>{t("privacy.toProcess")}</li>
+            <li>{t("privacy.toComm")}</li>
+            <li>{t("privacy.toImp")}</li>
           </ul>
         </div>
       </div>
@@ -40,54 +44,54 @@ const PrivacyPolicy = () => {
       <div>
 
         <div>
-          <h2>Information Sharing</h2>
+          <h2>{t("privacy.infoS")}</h2>
 
-          <p>We do not sell, trade, or otherwise transfer your personally identifiable information to outside parties. This does not include trusted third parties who assist us in operating our website or conducting our business, provided that these parties agree to keep this information confidential.</p>
+          <p>{t("privacy.infoP")}</p>
         </div>
       </div>
 
       <div>
 
         <div>
-          <h2>Data Security</h2>
+          <h2>{t("privacy.dataS")}</h2>
 
-          <p>We implement a variety of security measures to maintain the safety of your personal information when you submit a request or access your personal information.</p>
+          <p>{t("privacy.dataP")}</p>
         </div>
       </div>
 
       <div>
 
         <div>
-          <h2>Retention of Information</h2>
+          <h2>{t("privacy.retention")}</h2>
 
-          <p>We retain the information collected for as long as necessary to fulfill the purposes outlined in this Privacy Policy, unless a longer retention period is required or permitted by law.</p>
+          <p>{t("privacy.retentionP")}</p>
         </div>
       </div>
 
       <div>
 
         <div>
-          <h2>Third-Party Links</h2>
+          <h2>{t("privacy.third")}</h2>
 
-          <p>Our website may contain links to third-party websites. We are not responsible for the privacy practices or the content of these websites.</p>
+          <p>{t("privacy.thirdP")}</p>
         </div>
       </div>
 
       <div>
 
         <div>
-          <h2>Changes to the Privacy Policy</h2>
+          <h2>{t("privacy.changeOfP")}</h2>
 
-          <p>We reserve the right to update this Privacy Policy at any time. The effective date will be posted at the beginning of this policy.</p>
+          <p>{t("privacy.changeOfP1")}</p>
         </div>
       </div>
 
       <div>
 
         <div>
-          <h2>Contact Information</h2>
+          <h2>{t("privacy.contactI")}</h2>
 
-          <p>If you have any questions or concerns about our Privacy Policy, please contact us at <a className="chinese-red" href="mailto:Chaokaiqi@outlook.com.">Chaokaiqi@outlook.com.</a></p>
+          <p>{t("privacy.contactP")} <a className="chinese-red" href="mailto:Chaokaiqi@outlook.com.">Chaokaiqi@outlook.com.</a></p>
         </div>
       </div>
 
