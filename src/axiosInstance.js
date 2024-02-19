1da7e2032b914267771edd99061e10c0eba2da73
Md Antor <69227191+Antor1155@users.noreply.github.com>
2024-02-19 21:31:46 +0600

    translation on zh.json completed

--- src/axiosInstance.js
+++ src/axiosInstance.js
@@ -4,8 +4,14 @@ const instance = axios.create({
     baseURL: "http://localhost:3000"
 })
 
+// cyclic backend 
 // const instance = axios.create({
 //     baseURL: "https://good-rose-elephant-toga.cyclic.app"
+// })
+
+// render backend 
+// const instance = axios.create({
+//     baseURL: "https://chaokaiqi.onrender.com"
 // })
 
 
